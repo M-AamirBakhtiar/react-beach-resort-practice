a3fdb75de92d4313f8c205d09b2558d5fcbc247d
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-03-09 16:24:52 +0500

    Added Hero Component

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import Hero from '../components/Hero/Hero';
 
 export default function Rooms() {
-  return <div>Hello from Rooms</div>;
+  return <Hero hero="roomsHero" />;
 }
