239bd555b54c14d08823339458bc278b013df567
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-03-10 18:20:02 +0500

    Added Loading Room and Title rendering Logic

--- src/components/FeaturedRooms/FeaturedRooms.js
+++ src/components/FeaturedRooms/FeaturedRooms.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../../Context';
+import Loading from '../Loading/Loading';
+import Room from '../Room/Room';
+import Title from '../Title/Title';
 
 export default class FeaturedRooms extends Component {
+  static contextType = RoomContext;
   render() {
-    return <div>Hello from Featured Rooms.</div>;
+    let { loading, featuredRooms: rooms } = this.context;
+    rooms = rooms.map(room => <Room key={room.id} room={room} />);
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
