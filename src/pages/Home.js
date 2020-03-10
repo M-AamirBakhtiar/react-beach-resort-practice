ca1aa70979a40c724de940e17d5620335dae68e0
M-AamirBakhtiar <aamirbakhtiar.ab@gmail.com>
2020-03-10 16:03:11 +0500

    Added ContextAPI and FeaturedRooms Component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Hero from '../components/Hero/Hero';
 import Banner from '../components/Banner/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services/Services';
+import FeaturedRooms from '../components/FeaturedRooms/FeaturedRooms';
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 }
